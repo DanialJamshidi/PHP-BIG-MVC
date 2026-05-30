63956c4369b0b6d1519ccda4d0194480ae2d5140
Danial Jamshidi <danialjamshidi8877@gmail.com>
2026-05-30 22:56:31 +0330

    bugfix

--- app/core/routes/Route.php
+++ app/core/routes/Route.php
@@ -29,23 +29,25 @@ class Route
         return self::$routes;
     }
 
-    static public function Post($path, $controller, $method, $middleware = null)
-    {
-        if (strpos($path, ":number") !== false) {
-            $path = str_replace("/:number", "/([0-9]+)", $path);
-        }
-        if (strpos($path, ":all") !== false) {
-            $path = str_replace("/:all", '/([^"<>\\\\{}|^~$&/]+)', $path);
-        }
-        
-        $controllerClass = "app\\controllers\\" . $controller;
-        
-        self::$routes['POST'][$path] = [
-            'controller' => $controllerClass,
-            'method' => $method,
-            'middleware' => $middleware 
-        ];
-        
-        return self::$routes;
+static public function Post($path, $controller, $method, $middleware = null)
+{
+    $path = trim($path, '/');
+    
+    if (strpos($path, ":number") !== false) {
+        $path = str_replace("/:number", "/([0-9]+)", $path);
     }
+    if (strpos($path, ":all") !== false) {
+        $path = str_replace("/:all", '/([^"<>\\\\{}|^~$&/]+)', $path);
+    }
+    
+    $controllerClass = "app\\controllers\\" . $controller;
+    
+    self::$routes['POST'][$path] = [
+        'controller' => $controllerClass,
+        'method' => $method,
+        'middleware' => $middleware 
+    ];
+    
+    return self::$routes;
+}
 }
